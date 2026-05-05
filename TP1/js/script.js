a8b0ac7379c111b627638a36bdf0e3e23b32f63c
lourdes-millicay <lourdes.millicay@est.fi.uncoma.edu.ar>
2026-05-05 00:08:04 -0300

    Modificación en css tarjetas- bitacora
    modifique el css porque considero que esta bueno el estilo propuesto por nimia, solo cambio colores y agrego js para habilitar modo oscuro.
    agrego bitacora y muevo archivo script.js a carpeta js

--- TP1/js/script.js
+++ TP1/js/script.js
@@ -1,3 +1,38 @@
+//mensaje personalizado
 function mostrarMensaje() {
-    alert("¡Gracias por visitar mi tarjeta!");
+
+    const nombre = document.getElementById("nombre").innerText;
+
+    alert("Gracias por visitar la tarjeta de " + nombre);
 }
+
+// Seleccionamos el elemento del título en el header
+const headerTitle = document.querySelector('header h1');
+
+if (headerTitle) {
+
+    const hora = new Date().getHours();
+    let saludo;
+
+    if (hora < 12) {
+        saludo = "¡Buenos días!";
+    } else if (hora < 20) {
+        saludo = "¡Buenas tardes!";
+    } else {
+        saludo = "¡Buenas noches!";
+    }
+
+    // Cambiamos el texto dinámicamente
+    headerTitle.innerText = `${saludo} Somos PawCode`;
+
+}
+
+//cambiar de tema
+function cambiarTema() {
+
+    const tarjeta = document.getElementById("tarjeta");
+
+    tarjeta.classList.toggle("dark-mode");
+
+}
+
